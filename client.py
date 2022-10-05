6bf7050863c52b0ef79ae88aa9a31131cbe10da7
MatthewMTravers <MatthewMTravers@gmail.com>
2022-10-05 14:58:30 -0400

    Spacing, and updated comment

--- client.py
+++ client.py
@@ -34,16 +34,17 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price)/2
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     if price_b != 0:
-        return price_a/price_b
+        return price_a / price_b
     else:
-        print("Invalid price")
+        print("Cannot divide price by 0")
+
 
 # Main
 if __name__ == "__main__":
